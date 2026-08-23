cecd11272e2fe0ea5e6c5aa8e70eee4023991171
Abhisht Singh <80016041+Abhisht2000@users.noreply.github.com>
2026-08-23 14:57:47 +0530

    [88] [Time Beats: 100.00%] [Memory Beats: 74.44%] - LeetPush

--- 88-Merge-Sorted-Array.cpp
+++ 88-Merge-Sorted-Array.cpp
@@ -1,9 +1,16 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for(int i=m;i<m+n;i++){
-            nums1[i]=nums2[i-m];
+        int a=m-1;
+        int b=n-1;
+        int c=m+n-1;
+        while(b>=0){
+            if(a>=0 && nums1[a]>=nums2[b]){
+                nums1[c--]=nums1[a--];
+            }
+            else{
+                nums1[c--]=nums2[b--];
+            }
         }
-        sort(nums1.begin(),nums1.end());
     }
 };
